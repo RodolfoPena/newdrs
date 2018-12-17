5d6eedbd949f308f44a7f4841e7d8e5d7d8820a2
Rodolfo <rodolfo@npi8572a8.drs.drs.cl>
2018-12-17 11:50:48 -0300

    Devise gem added

---
+++ config/initializers/assets.rb
@@ -0,0 +1,16 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'img')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+Rails.application.config.assets.precompile += %w( socicon.css entypo.css theme.css *.svg *.woff *.wpff2 *.ttf *.png *.jpg)
